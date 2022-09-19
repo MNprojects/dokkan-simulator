baef0bb120c1337c3440d61ba6cb5db6219d0ed7
Michael Nash <michaelnash.work@gmail.com>
2022-09-19 16:55:03 +0100

    basic loop implemented. links doneish

--- src/dokkanSimulator.ts
+++ src/dokkanSimulator.ts
@@ -1,33 +1,118 @@
 export abstract class DokkanSimulator {
 
     static singleCharacterSimulation(character: Character, configOptions: SimConfiguration): (SimResults) {
-        let results: SimResults = { results: {}, team: {}, config: {} }
+        // TODO: validate input
+        let results: SimResults = { summary: {}, turnData: {}, team: {}, config: {} }
         let turns = {};
+        let appeared = 0;
+        let teamArray: any[] = [0, 1, 3, 4, 2, 5, 6];
+        let currentPosition = configOptions.startingPosition;
+        let simCharacter: any = character
 
-        for (let turn = 1; turn <= configOptions.turns; turn++) {
+        for (let turn = 1; appeared < configOptions.appearances; turn++) {
+            let currentRotation: any[] = []
+            // set which character slots are on rotation
+            if (turn % 2 === 0) {
+                currentRotation = currentRotation.concat(teamArray[2]).concat(teamArray[3]).concat(teamArray[((turn - 1) % 3) + 4]);
+            }
+            else {
+                currentRotation = currentRotation.concat(teamArray[0]).concat(teamArray[1]).concat(teamArray[((turn - 1) % 3) + 4]);
+            }
 
-            let turnName: string = 'turn ' + turn
-            // @ts-ignore
-            turns[turnName] = {};
-            console.log(turn);
+            // if the sim character is on rotation
+            if (currentRotation.includes(currentPosition)) {
+                appeared++;
+                currentPosition = currentRotation[configOptions.desiredPosition]
+                resetTurnStats(simCharacter);
+                configOptions.leaderSkill1(simCharacter);
+                configOptions.leaderSkill2(simCharacter);
+                simCharacter.startOfTurn()
+                simCharacter.turnStats.percentageStartOfTurnAttack += configOptions.percentageStartOfTurnAttack
+                activateLinks(simCharacter, configOptions.activeLinks)
+                console.log(simCharacter);
 
+                // Percentage - based leader skills
+                // Flat leader skills
+                // Percentage - based start of turn passives
+                // This is where start of turn + ATK support passives go.
+                // This is also where nuking style passives are factored in.
+                // Flat start of turn passives
+                // Percentage - based links
+                // Flat links
+                // All flat links are added in here, even if they say they don't activate until the unit supers (such as Kamehameha).
+                // Ki multiplier
+                // Build - up passives
+                // On Attack / on SA percentage - based passives
+                // On Attack / on SA flat passives
+                // SA multiplier
+                // SA - based ATK increases are factored in here as flat additions to the SA multiplier
+                character.startOfTurn();
+                let turnName: string = 'turn ' + turn;
+
+                // add results to turnData
+                // @ts-ignore
+                turns[turnName] = {
+                    appearanceCount: appeared
+                };
+                // reset
+            }
         }
-        results.results = turns
+        results.turnData = turns
 
         return results
     }
 }
 
-interface Character {
-    name: string
+
+export interface Character {
+    name: string,
+    startOfTurn(): any,
+    baseAttack: number,
+    categories: string[],
+    links: {}[],
 }
 
-interface SimConfiguration {
-    turns: number
+
+function resetTurnStats(character: any) {
+    character.turnStats = {
+        currentAttack: character.baseAttack,
+        percentageLeaderAttack: 1,
+        flatLeaderAttack: 0,
+        kiBoost: 0,
+        percentageStartOfTurnAttack: 1,
+        flatStartOfTurnAttack: 0,
+        percentageLinksBoostAttack: 1,
+    }
+}
+
+export interface SimConfiguration {
+    appearances: number,
+    startingPosition: number,
+    desiredPosition: number,
+    leaderSkill1(char: Character): void,
+    leaderSkill2(char: Character): void,
+    percentageStartOfTurnAttack: number,
+    flatStartOfTurnAttack: number,
+    activeLinks: string[]
+
 }
 
 interface SimResults {
-    results: Object,
+    summary: Object,
+    turnData: Object,
     team: Object,
     config: Object
 }
+
+
+
+function activateLinks(character: any, activeLinks: string[]) {
+    character.links.forEach((link: any) => {
+        if (activeLinks.includes(Object.keys(link)[0])) {
+            // @ts-ignore
+            Object.values(link)[0](character)
+        }
+    });
+}
+// TODO: builder pattern for config and character?
+
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,14 +1,45 @@
-import { DokkanSimulator } from '../src/index';
+import { DokkanSimulator, Character, SimConfiguration } from '../src/dokkanSimulator';
 import { deepEqual, equal } from "assert";
 
 
 describe('Single Character Simulation', function () {
     it('should have the same number of turns as given in the config', function () {
-        let testTurn = 4
-        let result = DokkanSimulator.singleCharacterSimulation({ name: 'Test' }, { turns: testTurn})
+        let expectedAppearances = 4
+        let character: Character =
+        {
+            name: 'Test',
+            startOfTurn() {
+                console.log('functiontest');
+            },
+            baseAttack: 10000,
+            categories: ["Super Saiyan"],
+            links: [{ "Super Saiyan": function (char: any) { char.turnStats.percentageLinksBoostAttack += 2.1} }],
+        }
+        let config: SimConfiguration = {
+            appearances: expectedAppearances,
+            startingPosition: 3,
+            desiredPosition: 1,
+            leaderSkill1(char: any) {
+                if (char.categories.includes("Super Saiyan")) {
+                    char.turnStats.percentageLeaderAttack += 1.7;
+                    char.turnStats.kiBoost += 3
+                }
+
+            },
+            leaderSkill2(char: any) {
+                if (char.categories.includes("Super Saiyan")) {
+                    char.turnStats.percentageLeaderAttack += 1.7;
+                    char.turnStats.kiBoost += 3
+                }
+            },
+            percentageStartOfTurnAttack: 0.4,
+            flatStartOfTurnAttack: 0,
+            activeLinks: ["Super Saiyan"],
+        }
+        let result = DokkanSimulator.singleCharacterSimulation(character, config)
         console.log(result);
-        
-        equal(Object.keys(result.results).length, testTurn);
+
+        equal(Object.keys(result.turnData).length, expectedAppearances);
     });
 });
 
